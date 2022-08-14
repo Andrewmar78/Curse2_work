f2e10e82abd3e339608cbd6e5df654bc331780d5
msi <Andrewmar78>
2022-08-14 14:37:15 +0500

    from windows 6

--- app.py
+++ app.py
@@ -71,6 +71,5 @@ def get_user_data_json(username):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(host='127.0.0.1', port='25000')
-
+    # app.run(debug=True)
+    app.run(host='127.0.0.1', port=80)
